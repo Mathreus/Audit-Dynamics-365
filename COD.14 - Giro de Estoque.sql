c8d4abd2e2db63583e87df2f1436dab540214f27
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2025-10-01 14:27:32 -0300

    Update COD.14 - Giro de Estoque.sql

--- COD.14 - Giro de Estoque.sql
+++ COD.14 - Giro de Estoque.sql
@@ -1,25 +1,25 @@
 SELECT
     P.COD_ITEM AS Cod_Item,
-    P.DESC_ITEM AS Descricao,
+    P.DESCR_ITEM AS Descricao,
     MAX(KDX.DATA) AS Ultima_compra,
     DATEDIFF(DAY, MAX(KDX.DATA), GETDATE()) AS dias_sem_giro,
     CASE
-	WHEN DATEDIFF(DAY, MAX(KDX.DATA), GETDATE()) < 180 THEN 'Produto OK'
-        WHEN DATEDIFF(DAY, MAX(KDX.DATA), GETDATE()) >= 180 AND <= 720 THEN 'Baixo Giro'
+        WHEN DATEDIFF(DAY, MAX(KDX.DATA), GETDATE()) < 180 THEN 'Produto OK'
+        WHEN DATEDIFF(DAY, MAX(KDX.DATA), GETDATE()) BETWEEN 180 AND 720 THEN 'Baixo Giro'
         WHEN DATEDIFF(DAY, MAX(KDX.DATA), GETDATE()) > 720 THEN 'Produto Obsoleto' 
         ELSE 'N/A'
-    END AS Giro_de_Estoque
+    END AS Giro_de_Estoque,
     CASE
-	WHEN QTD < 0 THEN 'Saída'
-	WHEN QTD > 0 THEN 'Entrada'
-	ELSE 'Sem_Movimentacao'
-    END Movimentacao
+        WHEN SUM(KDX.QTD) < 0 THEN 'Saída'
+        WHEN SUM(KDX.QTD) > 0 THEN 'Entrada'
+        ELSE 'Sem_Movimentacao'
+    END AS Movimentacao
 FROM
     VW_AUDIT_RM_ESTOQUE_PRODUTO AS P
 INNER JOIN
     VW_AUDIT_RM_GERENCIAMENTO_ESTOQUE AS KDX ON P.COD_ITEM = KDX.COD_ITEM
 WHERE
-    KDX.tipo_movimentacao = 'Entrada'
-    AND DEPOSITO LIKE '%BTN%'	
+    (P.DEPOSITO LIKE '%BTN%' OR KDX.DEPOSITO LIKE '%BTN%')
+    AND (P.COD_ESTABELECIMENTO = 'R351' OR KDX.COD_ESTABELECIMENTO = 'R351')
 GROUP BY
-    P.COD_ITEM, P.DESC_ITEM
+    P.COD_ITEM, P.DESCR_ITEM
